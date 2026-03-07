5a44f9307a44c2c88123ee8e761fc97949e1a9c0
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-03-07 09:11:23 +0700

    feat: inculding logo

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import Navigation from "./components/Navigation";
 export default function Home() {
   return (
     <div>
-      <Navigation />
       <h1 className="text-3xl font-bold">
         The wild oasis, welcome to paradise
       </h1>
