34a88296a9d23fb0d45ae3cbb01c272438c3d6eb
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-03-09 10:14:00 +0700

    feat: fetching dummy data in react components

--- src/app/cabins/page.js
+++ src/app/cabins/page.js
@@ -1,3 +1,16 @@
-export default function Page() {
-  return <h1>Cabins Pages</h1>;
+export default async function Page() {
+  // Fetching data in Components immidiatly
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return (
+    <div className="mt-10">
+      <h1>Cabins Pages</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
